00a50d30c47eaee38ddcb29bfee884295034629e
Eduardo Moreira <edumoreira462@gmail.com>
2025-04-07 20:48:23 -0300

    formulario andando

--- public/src/script.js
+++ public/src/script.js
@@ -5,6 +5,15 @@ import {
   addDoc,
 } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCWwxt3l9j8gnU_-mByHDb9_PWEKx5OZGk",
+  authDomain: "spa-sentirse-bien-403fe.firebaseapp.com",
+  projectId: "spa-sentirse-bien-403fe",
+  storageBucket: "spa-sentirse-bien-403fe.firebasestorage.app",
+  messagingSenderId: "27773872738",
+  appId: "1:27773872738:web:b11afa81ddec96b9343f3b"
+};
+
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
